f9b440241cc52ae1f6c21f1f6a0a753b82d16e4d
haris400008 <59230800+haris400008@users.noreply.github.com>
2023-06-11 19:28:52 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,109 @@
+
+import './App.css';
+import Line from './Line 14.svg'
+import vector from './vector .svg'
+import messenger from './Group9.svg'
+import Line1 from './Line 10.svg';
+function App() {
+  return (
+   <div className="request">
+    <div className="request-child" />
+    <div className="you-currently-have">You currently have 3 requests</div>
+    <div className="request-item" />
+    <div className="request-inner" />
+    <div className="st-judes-hospital-parent">
+      <b className="st-judes-hospital">{`St Judes Hospital   `}</b>
+      <div className="sarasotafl-33178">Sarasota,FL. 33178</div>
+      <div className="october">October</div>
+      <div className="december">December</div>
+      <div className="singles-10-doubles-container">
+        {`10 Singles, 10 `}
+        <span className="doubles">doubles</span>
+      </div>
+      <b className="b">10 - 17</b>
+      <b className="rooms">20 Rooms</b>
+      <img className="group-child" alt="" src={Line1} />
+    </div>
+    <div className="st-judes-hospital-group">
+      <b className="st-judes-hospital1">{`St Judes Hospital   `}</b>
+      <div className="sarasotafl-331781">Sarasota,FL. 33178</div>
+      <div className="october1">October</div>
+      <div className="december1">December</div>
+      <div className="singles-10-doubles-container1">
+        {`10 Singles, 10 `}
+        <span className="doubles">doubles</span>
+      </div>
+      <b className="b1">10 - 17</b>
+      <b className="rooms1">20 Rooms</b>
+      <img className="group-item" alt="" src={Line1} />
+     
+    </div>
+    <div className="rectangle-div" />
+    <div className="request-child1" />
+    <div className="received">RECEIVED</div>
+    <div className="negotiating">NEGOTIATING</div>
+    <div className="completed">COMPLETED</div>
+    <div className="completed1">COMPLETED</div>
+    <div className="line-div" />
+    <div className="request-child2" />
+    <div className="rectangle-parent">
+      <div className="group-inner" />
+      <i className="lodgn">LODGN</i>
+      <div className="current-requests">Current requests</div>
+      <div className="ongoing-stays">Ongoing stays</div>
+      <div className="previous-stays">Previous stays</div>
+      <div className="reports">Reports</div>
+      <div className="group-child1" />
+      <div className="log-out">Log - Out</div>
+      <img className="line-icon" alt="" src={Line} />
+      <img className="group-child2" alt="" src={Line} />
+      <img className="group-child3" alt="" src={Line} />
+      <img className="group-child4" alt="" src={Line} />
+      <div className="help-desk">Help-Desk:</div>
+      <div className="div">786-874 9988</div>
+    </div>
+    <div className="rectangle-group">
+      <div className="group-child5" />
+      <i className="holiday-inn">Holiday Inn</i>
+      <div className="group-child6" />
+      <img className="vector-icon" alt="" src={vector} />
+      <div className="miles-away-from">{`1.5 miles away from joblocation. `}</div>
+      <div className="singles-120">Singles: $120</div>
+      <div className="doubles-145">Doubles: $145</div>
+      <div className="group-child7" />
+      <div className="book-now">Book now</div>
+    </div>
+    <div className="rectangle-container">
+      <div className="group-child5" />
+      <i className="holiday-inn">Holiday Inn</i>
+      <div className="group-child6" />
+      <img className="vector-icon" alt="" src={vector} />
+      <div className="miles-away-from">{`1.5 miles away from joblocation. `}</div>
+      <div className="singles-120">Singles: $120</div>
+      <div className="doubles-145">Doubles: $145</div>
+      <div className="group-child7" />
+      <div className="book-now">Book now</div>
+    </div>
+    <div className="group-div">
+      <div className="rectangle-parent1">
+        <div className="group-child12" />
+        <i className="holiday-inn2">Holiday Inn</i>
+        <div className="group-child13" />
+        <img className="group-child14" alt="" src={vector}/>
+        <div className="miles-away-from2">{`1.5 miles away from joblocation. `}</div>
+        <div className="singles-1202">Singles: $120</div>
+        <div className="doubles-1452">Doubles: $145</div>
+        <div className="group-child15" />
+        <div className="book-now2">Book now</div>
+        <div className="group-child16" />
+        <div className="rare-find">Rare Find</div>
+      </div>
+    </div>
+    <div className="request-child3" />
+    <img class="group-icon" alt="" src={messenger} />
+
+  </div>
+  );
+}
+
+export default App;
